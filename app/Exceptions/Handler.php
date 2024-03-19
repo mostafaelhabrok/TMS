1c843b7e58c6d97f6d6e1c80e306286d2aceddba
mostafa <mostafa.elhabrok@gmail.com>
2024-03-19 22:41:59 +0200

    handle exceptions as json

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,4 +45,16 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $exception
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Throwable $exception)
+    {
+        return response()->json(['message' => $exception->getMessage()] , 500 );
+    }
 }
